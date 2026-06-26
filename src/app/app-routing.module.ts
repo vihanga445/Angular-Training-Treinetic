f13a14e22fa0d9506cfe766d04049388c8b7da82
Vihanga Abeynayake <vihangaabeynayaka789@gmail.com>
2026-06-26 13:38:25 +0530

    Revert "adding modules to the app"
    This reverts commit 44cd9fa02fbbaf02ce7631cd166be877ece68e19.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,8 +18,6 @@ const routes: Routes = [
     component: TodoDetailComponent,
     canActivate: [authGuard],
   },
-  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
-  { path: 'todos', loadChildren: () => import('./todo/todo.module').then(m => m.TodoModule) },
   { path: '**', redirectTo: '/todos' },
 ];
 
